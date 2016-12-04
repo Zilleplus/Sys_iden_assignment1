927325ebd05168dba7cc544f3ff0d1ebba881137
Marie Valenduc <marievalenduc@MacBook-Air-de-Marie.local>
2016-12-04 13:09:21 +0100

    add optimal order + test error

--- Sess2_test.m
+++ Sess2_test.m
@@ -1,19 +1,20 @@
 %--------------------------------------------------------------------------
-% Marie Valenduc and Willem Melis (November 2016) 
+% Marie Valenduc and Willem Melis (November 2016)
 % System identification and modeling - Session 2
 %--------------------------------------------------------------------------
-clear all; close all;
-
+%clear all; close all;
+rng(1)
+tic
 % data
 N_est = 1000;                               % size of the estimation set
 N_val = 10000;                              % size of the validation set
-N = N_est + N_val; 
+N = N_est + N_val;
 index_est = 1:N_est;
 index_val = N_est+1:N;
 stdev_u0 = 1;
 stdev_ny = 0.5;
 
-for k =1:2:3
+for k =1
     % generation of the data
     [b,a] = cheby1(3,0.5,[2*0.15 2*0.3]);   % transfer function G0
     u0 = stdev_u0*randn(N,1);               % input
@@ -32,52 +33,44 @@ for k =1:2:3
         
         % output of the FIR system
         y_hat = filter(g,1,u0);
-
+        
         % computing the least squares cost functions V_LS
         % based on the estimation set
-        V_LS_est(order) = sum((y(index_est) - y_hat(1:N_est)).^2)/N_est; 
+        V_LS_est(order) = sum((y(index_est) - y_hat(1:N_est)).^2)/N_est;
         % based on the validation set
         V_LS_val(order) = sum((y(index_val) - y_hat(N_est+1:N_est+N_val)).^2)/N_val;
         
         % computing the Akaike cost function V_AIC
-        V_AIC(order) = V_LS_val(order)*(1+2*(order+1)/N_val);
+        V_AIC(order) = V_LS_val(order)*(1+2*(order)/N_val);
         
         % computing the error V_0
-        V_0(order) = sum((y0(index_val) - y_hat(index_val)).^2)/N_val;       
+        V_0(order) = sum((y0(index_val) - y_hat(index_val)).^2)/N_val;
     end
     
     disp(['- - - Results for sigma_ny = ',num2str(stdev_ny)]);
-    [~,indexmin] = min(V_AIC);
-    disp(['The optimal order given by the AIC is ',num2str(indexmin)]);
+    [~,indexmin] = min(V_LS_est);
+    disp(['The optimal order is ',num2str(indexmin)]);
     
-    [~,indexmin] = min(V_LS_val);
-    disp(['The optimal order given by the validation set is ',num2str(indexmin)]);
-    
-    figure(1); subplot(2,2,k)
+    figure(2); subplot(1,2,k)
     plot(V_LS_est./stdev_ny^2,'r','LineWidth',2); hold all;
     plot(V_LS_val./stdev_ny^2,'g','LineWidth',2); hold all;
     plot(V_AIC./stdev_ny^2,'LineWidth',2)
     set(gca, 'fontsize', 17);
-    xlim([0,100]); ylim([0.7, 1.2]);
+    xlim([0,order]);
     ylabel('Cost'); xlabel('Order');
     legend('V_{est}','V_{val}','V_{AIC}');
-    if(k==1) title('Noisy data; \sigma_{ny} = 0.5'); 
-    elseif(k==3) title('Noisy data; \sigma_{ny} = 0.05'); 
-    end
     
-    figure(1); subplot(2,2,k+1)
+    
+    figure(2); subplot(1,2,2)
     plot(sqrt(V_0/stdev_ny^2),'LineWidth',2)
     set(gca, 'fontsize', 17);
-    xlim([0,100]); ylim([0, 1]);
+    xlim([0,order]);
     ylabel('Normalized RMS'); xlabel('Order');
     legend('V_0');
-    if(k==1) title('Noiseless data; \sigma_{ny} = 0.5'); 
-    elseif(k==3) title('Noiseless data; \sigma_{ny} = 0.05'); 
-    end
     
-    stdev_ny = 0.05;                        % for second experiment
+    
 end
-
+toc
 
 
 

bdebfc993cc8bb1aa921c4feb454499889f88e5c
Zilleplus <zilleplus@gmail.com>
2016-12-24 15:30:18 +0100

    final mods

--- identification_arx.m
+++ identification_arx.m
@@ -1,14 +1,21 @@
 run('identification_init');
-%% Balanced model reduction
+%% optimal value selected from table
 close all;
-na = 20; nb = 20;
+na = 15; nb = 24;
 
 [model, order, fit, aic_value, mag1, wout1 ]= fun_arx_model(na,nb, ...
     preprocessed_prbs_est, preprocessed_prbs_val,true );
 
 figureNumber=5;
 fun_bode_plot(wout1,mag1,'ARX model',figureNumber);
-%%
+%% Balanced model reduction
+close all;
+na = 20; nb = 20;
+
+[model, order, fit, aic_value, mag, wout ]= fun_arx_model(na,nb, ...
+    preprocessed_prbs_est, preprocessed_prbs_val,true );
+
+
 sys = ss(model);  
 [Ab,Bb,Cb,M,T] = dbalreal(sys.a,sys.b,sys.c); 
 
@@ -17,13 +24,13 @@ figure(1); clf; bar(M); title('Hankel Singular Values');
 xlabel('State'); ylabel('State Energy');
 
 % Reduced order
-rorder = 13;
+rorder = 12;
 Ab = Ab(1:rorder,1:rorder);
 Bb = Bb(1:rorder);
 Cb = Cb(1:rorder);
 rsys = ss(Ab,Bb,Cb,sys.D,1);
 
-[ry,fit] = compare(preprocessed_prbs_val, idpoly(rsys))
+[ry,fitreduced] = compare(preprocessed_prbs_val, idpoly(rsys))
 
 figure(2); clf; grid on;
 plot(ry.y); hold on;

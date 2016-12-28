ba79c727c8648e56d75fc6d88734225a0987a8cd
Zilleplus <zilleplus@gmail.com>
2016-12-28 23:45:29 +0100

    added bodeplot to subspace

--- identification_subspace.m
+++ identification_subspace.m
@@ -3,6 +3,8 @@ run('identification_init');
 % Subspace model
 close all; disp('Testing SUBSPACE model ...');
 nx = 3:20;
-[model, fit, aic_value ] = fun_subspace_model(nx,...
+[model, fit, aic_value, mag, wout  ] = fun_subspace_model(nx,...
     preprocessed_prbs_est, preprocessed_prbs_val, true)
 
+figureNumber=5;
+fun_bode_plot(wout,mag,'subspace model',figureNumber);
